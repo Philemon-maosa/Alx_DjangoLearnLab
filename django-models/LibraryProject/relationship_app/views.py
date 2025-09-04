8f716b03f9641dfb46082addce121b3cb4b8f5eb
philemon <philemonmaosa@gmail.com>
2025-09-04 09:12:24 +0300

    I have edited the file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from .models import Book, Library
 from django.contrib.auth.decorators import user_passes_test, permission_required
 from django.http import HttpResponse
+from .models import Book, Library
+
 
 
 # Function-based view to list all books
