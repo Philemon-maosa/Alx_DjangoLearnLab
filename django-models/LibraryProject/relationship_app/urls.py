4e671da5b35cac43739cd1859a3e2a19467e9c80
philemon <philemonmaosa@gmail.com>
2025-09-04 08:33:28 +0300

    Added the url patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("books/", views.book_list, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-    # 🔹 Authentication URLs
+    # Authentication URLs
     path("register/", views.register, name="register"),
     path(
         "login/",
@@ -18,4 +18,9 @@ urlpatterns = [
         LogoutView.as_view(template_name="relationship_app/logout.html"),
         name="logout",
     ),
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
 ]
+
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from .models import Book
 from .models import Library
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
+
 
 
 # Function-based view to list all books
@@ -35,3 +38,35 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
+
+
+#  Role check helpers
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+#  Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Welcome Admin! You have full control.")
+
+
+#  Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return HttpResponse("Welcome Librarian! You can manage books and libraries.")
+
+
+# Member View
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("Welcome Member! You can browse books.")
+
