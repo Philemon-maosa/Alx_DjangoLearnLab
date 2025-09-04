0e76c9a646b0593a47b1eb37a2d67dc7a1d39202
philemon <philemonmaosa@gmail.com>
2025-09-04 08:44:10 +0300

    I have edited admin_view.py

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponse
 
+
 def is_admin(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
+
 @user_passes_test(is_admin)
 def admin_view(request):
-    return HttpResponse("Welcome Admin! You have full control.")
+    return HttpResponse("Admin View: Only Admins can access this page.")
