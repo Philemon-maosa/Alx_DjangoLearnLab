6539ab72180248dbd52529c6e9c5cfca173b7a32
philemon <philemonmaosa@gmail.com>
2025-09-10 10:30:36 +0300

    Added views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 # Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Add your form handling logic here
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
