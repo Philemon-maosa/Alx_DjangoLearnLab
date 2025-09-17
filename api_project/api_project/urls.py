dbff417072fb2f94614cf165391c34913ee9b5e8
philemon <philemonmaosa@gmail.com>
2025-09-17 10:56:23 +0300

    Edited the urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include   # include is needed here
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # this connects your app’s urls.py
+    path('api/', include('api.urls')),
 ]
