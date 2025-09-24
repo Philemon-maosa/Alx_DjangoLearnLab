9a51f86b60cdbca4c477bd3d9f5aa9ce0c1535aa
philemon <philemonmaosa@gmail.com>
2025-09-24 17:46:24 +0300

    Edited the api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated  # <-- add this
 from .models import Book
 from .serializers import BookSerializer
+
 
 
 # List all books
