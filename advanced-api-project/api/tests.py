a166a0e8b7a83b3ecae6c1db690c2bee762b7bed
philemon <philemonmaosa@gmail.com>
2025-09-26 06:34:35 +0300

    Edited the test.py

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -41,6 +41,7 @@ class BookAPITests(APITestCase):
         self.list_url = reverse("book-list")   # /books/
         self.detail_url = reverse("book-detail", kwargs={"pk": self.book.id})
 
+    # ---------- READ ----------
     def test_list_books(self):
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -51,13 +52,44 @@ class BookAPITests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["title"], "Book One")
 
+    # ---------- CREATE ----------
     def test_create_book_authenticated(self):
         self.client.login(username="tester", password="pass1234")
         data = {"title": "New Book", "publication_year": 2022, "author": self.author.id}
         response = self.client.post(self.list_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
 
     def test_create_book_unauthenticated(self):
         data = {"title": "Unauthorized Book", "publication_year": 2022, "author": self.author.id}
         response = self.client.post(self.list_url, data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Book.objects.count(), 1)
+
+    # ---------- UPDATE ----------
+    def test_update_book_authenticated(self):
+        self.client.login(username="tester", password="pass1234")
+        data = {"title": "Updated Book", "publication_year": 2021, "author": self.author.id}
+        response = self.client.put(self.detail_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, "Updated Book")
+
+    def test_update_book_unauthenticated(self):
+        data = {"title": "Blocked Update", "publication_year": 2021, "author": self.author.id}
+        response = self.client.put(self.detail_url, data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.book.refresh_from_db()
+        self.assertNotEqual(self.book.title, "Blocked Update")
+
+    # ---------- DELETE ----------
+    def test_delete_book_authenticated(self):
+        self.client.login(username="tester", password="pass1234")
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_delete_book_unauthenticated(self):
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Book.objects.count(), 1)
