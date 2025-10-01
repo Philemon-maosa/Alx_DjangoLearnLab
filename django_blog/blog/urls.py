7c3702c6f27a083fd3b1434c221d6c445fd1df50
philemon <philemonmaosa@gmail.com>
2025-10-01 16:22:35 +0300

    edited urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 
-    # Tag filtering
-    path('tag/<slug:slug>/', views.tagged_posts, name='tagged-posts'),
+    # Tag filtering (updated)
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
 
     # Search
     path('search/', views.search_posts, name='search-posts'),
