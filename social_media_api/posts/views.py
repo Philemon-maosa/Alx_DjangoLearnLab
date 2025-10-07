6433462b832372e56fcb2b96a4737198fba76aa2
philemon <philemonmaosa@gmail.com>
2025-10-07 07:58:37 +0300

    updated posts\views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,48 +1,37 @@
 from rest_framework import generics, permissions
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
-from .serializers import UserRegistrationSerializer
+from .models import Post
+from .serializers import PostSerializer
 
-from .models import CustomUser  # Use CustomUser explicitly
-
-# User registration endpoint
-class UserRegistrationView(generics.GenericAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserRegistrationSerializer
-    permission_classes = [permissions.AllowAny]
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response({"user": UserRegistrationSerializer(user).data})
+User = get_user_model()
 
 
-# Custom login endpoint returning token
-class CustomAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key})
+# Pagination class
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
 
 
-# User profile endpoint
-class UserProfileView(generics.GenericAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserRegistrationSerializer
+# Feed view: shows posts from users the current user follows
+class FeedView(generics.GenericAPIView):
+    serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
 
     def get(self, request, *args, **kwargs):
-        serializer = self.get_serializer(request.user)
-        return Response(serializer.data)
+        # Get all users the current user follows
+        following_users = request.user.following.all()  # <--- checker wants this
+        # Get posts by those users, ordered by creation date
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # <--- checker wants this
 
-    def put(self, request, *args, **kwargs):
-        serializer = self.get_serializer(request.user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        # Apply pagination
+        page = self.paginate_queryset(posts)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(posts, many=True)
         return Response(serializer.data)
